9a42547e8cbf98f0b0e30d75076244a4d3e0e4fd
Anik-the-dev <niksctg00@gmail.com>
2022-04-07 21:29:39 +0600

    calculate Done and show the result

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,6 +4,8 @@ import InputBox from './InputBox';
 
 const Input = () => {
     const primaryData = [{ courseNmae: '', cgpa: 1, credit: 1 }]
+    const [totalCredit, setTotalCredit] = useState(0)
+    const [totalCgpa, setTotalCgpa] = useState(0)
     const [inputText, setInputText] = useState(primaryData)
     const handleAddInputBoxClick = () => {
         let newInputText = [...inputText, { courseNmae: '', cgpa: 1, credit: 1 }]
@@ -15,7 +17,7 @@ const Input = () => {
         e.preventDefault()
         let selected = e.target.name
         let newData = [...inputText]
-        newData[index][selected] = e.target.value
+        newData[index][selected] = parseFloat(e.target.value)
         setInputText(newData)
 
     }
@@ -24,18 +26,34 @@ const Input = () => {
         e.preventDefault()
         let selected = e.target.name
         let newDataCredit = [...inputText]
-        newDataCredit[index][selected] = e.target.value
+        newDataCredit[index][selected] = parseFloat(e.target.value)
         setInputText(newDataCredit)
 
     }
 
-    console.log(inputText)
+    // handle calculate btn..........
+    function handleCalculateBtn(totalCgpa,totalCredit){
+        for(const i of inputText){
+            totalCgpa+=i.cgpa * i.credit
+            totalCredit+=i.credit
+            
+        }
+        setTotalCredit(totalCredit)
+        let cgpa = totalCgpa/totalCredit
+        setTotalCgpa( cgpa.toFixed(2))
+
+    
+
+    }
+
+
+   
 
 
     return (
         <div>
             <header className='header'>
-                <h1 className='text-center'>Anik's CGPA Calculator</h1>
+                <h1 className='text-center'>Anik's CGPA Calculator {totalCgpa}</h1>
             </header>
 
             {/* inputbox calling */}
@@ -47,7 +65,8 @@ const Input = () => {
             {/* Add more btn */}
             <div className='container'>
                 <button className='btn' onClick={handleAddInputBoxClick}><p>Add One More Course</p></button>
-                <button className='btn btn-outline'><p>Calculate Your CGPA</p></button>
+            {/* Calculate Btn */}
+                <button className='btn btn-outline' onClick={()=>handleCalculateBtn(totalCgpa,totalCredit)}><p>Calculate Your CGPA</p></button>
             </div>
 
 
