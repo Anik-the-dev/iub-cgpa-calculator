ccfbfa3b1ebca3ec5364482cedb3688033e5a4b4
Anik-the-dev <niksctg00@gmail.com>
2022-04-07 12:03:49 +0600

    input Box Done

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -3,7 +3,37 @@ import React from 'react';
 const InputBox = () => {
     return (
         <div className='card container'>
-            <h1>anol</h1>
+            <label>Course Name: </label>
+            <input type='text' placeholder='Optional'></input>
+            <label>Obtained Grade: </label>
+            <select>
+                <option value='A'>A</option>
+                <option value='A-'>A-</option>
+                <option value='B+'>B+</option>
+                <option value='B'>B</option>
+                <option value='B-'>B-</option>
+                <option value='C+'>C+</option>
+                <option value='C'>C</option>
+                <option value='C-'>C-</option>
+                <option value='D+'>D+</option>
+                <option value='D'>D</option>
+                <option value='F'>F</option>
+            </select>
+
+            <br/><label>Earned Credit: </label>
+            <select>
+                <option value='0'>0</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+                <option value='6'>6</option>
+                <option value='7'>7</option>
+                <option value='8'>8</option>
+                <option value='9'>9</option>
+            
+            </select>
             
         </div>
     );
