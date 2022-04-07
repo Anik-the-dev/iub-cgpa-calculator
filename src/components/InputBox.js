ef1637c65e914fa90e507f7e3a3c6f253054ff8a
Anik-the-dev <niksctg00@gmail.com>
2022-04-07 12:31:57 +0600

    input Box Style Done

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -2,39 +2,56 @@ import React from 'react';
 
 const InputBox = () => {
     return (
-        <div className='card container'>
-            <label>Course Name: </label>
-            <input type='text' placeholder='Optional'></input>
-            <label>Obtained Grade: </label>
-            <select>
-                <option value='A'>A</option>
-                <option value='A-'>A-</option>
-                <option value='B+'>B+</option>
-                <option value='B'>B</option>
-                <option value='B-'>B-</option>
-                <option value='C+'>C+</option>
-                <option value='C'>C</option>
-                <option value='C-'>C-</option>
-                <option value='D+'>D+</option>
-                <option value='D'>D</option>
-                <option value='F'>F</option>
-            </select>
+        <div className='container'>
+            <div className="card input-box">
+                <div className='flex'>
+                    <div className='courseName'>
+                        <label>Course Name: </label>
+                        <input type='text' placeholder='Optional'></input>
+                    </div>
 
-            <br/><label>Earned Credit: </label>
-            <select>
-                <option value='0'>0</option>
-                <option value='1'>1</option>
-                <option value='2'>2</option>
-                <option value='3'>3</option>
-                <option value='4'>4</option>
-                <option value='5'>5</option>
-                <option value='6'>6</option>
-                <option value='7'>7</option>
-                <option value='8'>8</option>
-                <option value='9'>9</option>
-            
-            </select>
-            
+                    <div className='obtainedGrade'>
+                        <label>Obtained Grade: </label>
+                        <select>
+                            <option value='A'>A</option>
+                            <option value='A-'>A-</option>
+                            <option value='B+'>B+</option>
+                            <option value='B'>B</option>
+                            <option value='B-'>B-</option>
+                            <option value='C+'>C+</option>
+                            <option value='C'>C</option>
+                            <option value='C-'>C-</option>
+                            <option value='D+'>D+</option>
+                            <option value='D'>D</option>
+                            <option value='F'>F</option>
+                        </select>
+                    </div>
+
+                    <div className='earnedCredit'>
+                        <label>Earned Credit: </label>
+                        <select>
+                            <option value='0'>0</option>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                            <option value='5'>5</option>
+                            <option value='6'>6</option>
+                            <option value='7'>7</option>
+                            <option value='8'>8</option>
+                            <option value='9'>9</option>
+
+                        </select>
+
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+
         </div>
     );
 };
