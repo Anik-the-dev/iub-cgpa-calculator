94e91dfd2c1a23852441914e16a4987262bafb4e
Anik-the-dev <niksctg00@gmail.com>
2022-04-07 18:43:55 +0600

    all btn style done

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import InputBox from './InputBox';
 
+
 const Input = () => {
-    const primaryData = [{ courseNmae : '', cgpa: 1, credit:1 }]
+    const primaryData = [{ courseNmae: '', cgpa: 1, credit: 1 }]
     const [inputText, setInputText] = useState(primaryData)
     const handleAddInputBoxClick = () => {
-        let newInputText = [...inputText,{ courseNmae : '', cgpa: 1, credit:1 }]
+        let newInputText = [...inputText, { courseNmae: '', cgpa: 1, credit: 1 }]
         setInputText(newInputText)
 
 
@@ -16,14 +17,18 @@ const Input = () => {
             <header className='header'>
                 <h1 className='text-center'>Anik's CGPA Calculator</h1>
             </header>
-            
+
             {/* inputbox calling */}
             {
-                inputText.map(() =><InputBox></InputBox>)
+                inputText.map(() => <InputBox></InputBox>)
             }
-            
+
             {/* Add more btn */}
-            <button onClick={handleAddInputBoxClick}>Add One More</button>
+            <div className='container'>
+                <button className='btn' onClick={handleAddInputBoxClick}><p>Add One More Course</p></button>
+                <button className='btn btn-outline'><p>Calculate Your CGPA</p></button>
+            </div>
+
 
 
 
